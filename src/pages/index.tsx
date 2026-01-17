bb28c35e8e1cb0889c3cc4121916b66aaa60d673
Andrew Buter <andrey.buter@gmail.com>
2026-01-18 00:13:26 +0100

    open playground on open app,

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,53 +1,6 @@
-import React from "react";
-import clsx from "clsx";
-import Layout from "@theme/Layout";
-import Link from "@docusaurus/Link";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import styles from "./index.module.scss";
-import { HomepageFeatures } from "@site/src/components/HomepageFeatures";
-import { AlphaTabFull } from "@site/src/components/AlphaTabFull";
-
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/introduction"
-          >
-            Get Started
-          </Link>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/playground"
-          >
-            Explore Playground
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import React from 'react';
+import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <Layout title={siteConfig.tagline} description={siteConfig.tagline}>
-      <HomepageHeader />
-      <main className="main-at">
-        <AlphaTabFull
-          settings={{
-            core: {
-              file: "/files/canon.gp",
-              tracks: [0],
-            },
-          }}
-        />
-        <HomepageFeatures />
-      </main>
-    </Layout>
-  );
+  return <Redirect to="/docs/playground" />;
 }
